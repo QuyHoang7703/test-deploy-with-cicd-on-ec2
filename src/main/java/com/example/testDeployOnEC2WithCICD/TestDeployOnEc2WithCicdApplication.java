1eb7a0e188e5f05374f6e99218743f351091ad21
QuyHoang7703 <hoangvanquy772003@gmail.com>
2024-10-24 09:13:28 +0700

    initial commit

---
+++ src/main/java/com/example/testDeployOnEC2WithCICD/TestDeployOnEc2WithCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.testDeployOnEC2WithCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeployOnEc2WithCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeployOnEc2WithCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testDeployOnEC2WithCICD/TestDeployOnEc2WithCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testDeployOnEC2WithCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployOnEc2WithCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
